cc6f9f2b93fc4dcf4ecd975c19c89e5471812140
Julie McDonald <julie@julies-mac-mini.home>
2024-10-12 11:29:04 +0100

    add favicon

--- scripts/favicon.js
+++ scripts/favicon.js
@@ -13,6 +13,7 @@ const FAVICON = `
 <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
+<link rel="icon" type="image/x-icon" href="/icon/favicon.ico">
 <link rel="manifest" href="/icon/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png">
